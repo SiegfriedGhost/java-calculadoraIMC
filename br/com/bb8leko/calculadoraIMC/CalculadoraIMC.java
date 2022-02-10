f439d648d9e04944ea43077192d0f4b5fd0dc55e
Leandro de Oliveira Torres Santos <leandro.ots@bb.com.br>
2022-02-10 20:59:48 -0300

    Adicionando terceiro comentario para teste git

--- br/com/bb8leko/calculadoraIMC/CalculadoraIMC.java
+++ br/com/bb8leko/calculadoraIMC/CalculadoraIMC.java
@@ -1,16 +1,16 @@
 package br.com.bb8leko.calculadoraIMC;
 
-//Calculadora IMC 
+//Calculadora IMC
 public class CalculadoraIMC {
-	
+	//Método IMC
     private double imc(double altura, double peso) {
     	double alt = altura / 100;
     	double imc = peso / (alt * alt);
     	return imc; 
     }
-
+    //Método Main
     public static void main(String[] args) {
 		CalculadoraIMC calc = new CalculadoraIMC();
-		System.out.println("IMC: " + calc.imc(162, 78));
+		System.out.println("IMC: " + calc.imc(170, 78));
 	}
 }
