868efc811ffba7c968b422e88121ca7c4f6645cc
Leandro de Oliveira Torres Santos <leandro.ots@bb.com.br>
2022-02-10 21:01:35 -0300

    Tirando acentos

--- br/com/bb8leko/calculadoraIMC/CalculadoraIMC.java
+++ br/com/bb8leko/calculadoraIMC/CalculadoraIMC.java
@@ -2,13 +2,13 @@ package br.com.bb8leko.calculadoraIMC;
 
 //Calculadora IMC
 public class CalculadoraIMC {
-	//Método IMC
+	//Metodo IMC
     private double imc(double altura, double peso) {
     	double alt = altura / 100;
     	double imc = peso / (alt * alt);
     	return imc; 
     }
-    //Método Main
+    //Metodo Main
     public static void main(String[] args) {
 		CalculadoraIMC calc = new CalculadoraIMC();
 		System.out.println("IMC: " + calc.imc(170, 78));
